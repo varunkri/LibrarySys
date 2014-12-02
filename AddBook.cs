bcf2aa4edebae1244035c817387b2a17922d37cb
Chen Yang <thechenyang@gmail.com>
2014-12-02 09:02:58 -0800

    Add book UI

---
+++ AddBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LibraryApp.Classes
+{
+    public partial class AddBook : Form
+    {
+        public AddBook()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
